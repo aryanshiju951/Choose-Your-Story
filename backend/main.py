fa12603487055f0b18c78274742478e1a54bc3f7
Aryan Shiju <aryanshijude@gmail.com>
2025-12-30 15:00:33 +0530

    Initial commit

---
+++ backend/main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from core.config import settings
+from routers import story,job
+from db.database import create_tables
+create_tables()
+app=FastAPI(
+    title="Choose Your Story Game API",
+    description="api to generate cool stories",
+    version="1.0.0",
+    docs_url="/docs",
+    redoc_url="/redocs"
+)
+app.include_router(story.router,prefix=settings.API_PREFIX)
+app.include_router(job.router,prefix=settings.API_PREFIX)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.ALLOWED_ORIGINS,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"] 
+)
+if __name__=="__main__":
+    import uvicorn
+    uvicorn.run(app,host="0.0.0.0",port=8000,reload=True)
